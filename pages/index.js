692ce709e6d8efa7abea8f40f3f6eacba0641534
Hope Tambala <hopetambala@outlook.com>
2020-01-27 01:28:17 -0500

    feat: add eslint to project!

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function Index () {
+  return (
+    <div>
+      <h1>Hello Next.js 👋</h1>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  )
+}
