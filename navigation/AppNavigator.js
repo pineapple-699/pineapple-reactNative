a1ac42e0c5737896518074e908c1bbec7d45160f
Chance Murphy <46692350+chance-murphy@users.noreply.github.com>
2020-02-06 10:59:19 -0500

    chore: Nav is set up. Minor edits still needed

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,59 @@
+//General Imports
 import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
+// Tab Nav Import
 import MainTabNavigator from './MainTabNavigator';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
-);
+// Screen Imports
+import { loadingScreen } from '../screens/AppLoading';
+import { LogInScreen } from '../screens/SplashScreen';
+import { SignUpScreen } from '../screens/SignUpScreen';
+
+const LoadNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: loadingScreen,
+      navigationOptions: {
+        header: null,
+      }
+    }
+  },
+  {
+    initialRouteName: 'Home',
+  }
+)
+
+const StackNavigator = createStackNavigator(
+  {
+    LogIn: {
+      screen: LogInScreen,
+      navigationOptions: {
+        header: null,
+      }
+    },
+    SignUp: {
+      screen: SignUpScreen,
+      navigationOptions: {
+        header: null,
+      }
+    }
+  }
+)
+
+const App = createSwitchNavigator({
+  // You could add another route here for authentication.
+  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  Load: {
+    screen: LoadNavigator,
+  },
+  Auth: {
+    screen: StackNavigator,
+  },
+  Main: {
+    screen: MainTabNavigator,
+  }
+});
+
+export default createAppContainer(App);
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,66 +12,69 @@ import {
 
 import { MonoText } from '../components/StyledText';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}>
-        <View style={styles.welcomeContainer}>
-          <Image
-            source={
-              __DEV__
-                ? require('../assets/images/robot-dev.png')
-                : require('../assets/images/robot-prod.png')
-            }
-            style={styles.welcomeImage}
-          />
-        </View>
+export class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-        <View style={styles.getStartedContainer}>
-          <DevelopmentModeNotice />
-
-          <Text style={styles.getStartedText}>Get started by opening</Text>
-
-          <View
-            style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-            <MonoText>screens/HomeScreen.js</MonoText>
+  render() {
+    return (
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}>
+          <View style={styles.welcomeContainer}>
+            <Image
+              source={
+                __DEV__
+                  ? require('../assets/images/robot-dev.png')
+                  : require('../assets/images/robot-prod.png')
+              }
+              style={styles.welcomeImage}
+            />
           </View>
 
-          <Text style={styles.getStartedText}>
-            Change this text and your app will automatically reload.
+          <View style={styles.getStartedContainer}>
+            <DevelopmentModeNotice />
+
+            <Text style={styles.getStartedText}>Get started by opening</Text>
+
+            <View
+              style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
+              <MonoText>screens/HomeScreen.js</MonoText>
+            </View>
+
+            <Text style={styles.getStartedText}>
+              Change this text and your app will automatically reload.
           </Text>
-        </View>
+          </View>
 
-        <View style={styles.helpContainer}>
-          <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
-            <Text style={styles.helpLinkText}>
-              Help, it didn’t automatically reload!
+          <View style={styles.helpContainer}>
+            <TouchableOpacity onPress={handleHelpPress} style={styles.helpLink}>
+              <Text style={styles.helpLinkText}>
+                Help, it didn’t automatically reload!
             </Text>
-          </TouchableOpacity>
-        </View>
-      </ScrollView>
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
 
-      <View style={styles.tabBarInfoContainer}>
-        <Text style={styles.tabBarInfoText}>
-          This is a tab bar. You can edit it in:
+        <View style={styles.tabBarInfoContainer}>
+          <Text style={styles.tabBarInfoText}>
+            This is a tab bar. You can edit it in:
         </Text>
 
-        <View
-          style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-          <MonoText style={styles.codeHighlightText}>
-            navigation/MainTabNavigator.js
+          <View
+            style={[styles.codeHighlightContainer, styles.navigationFilename]}>
+            <MonoText style={styles.codeHighlightText}>
+              navigation/MainTabNavigator.js
           </MonoText>
+          </View>
         </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
+  
 
 function DevelopmentModeNotice() {
   if (__DEV__) {
