65c5baf59d646b8b30f88a80fc2ec1f4f963cd5f
Hope Tambala <hopetambala@outlook.com>
2020-02-14 17:32:19 -0500

    fix: remove styled text

--- components/StyledText.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-
-export default function MonoText(props) {
-  const { style } = props;
-  return (
-    <Text style={[style, { fontFamily: 'space-mono' }]} />
-  );
-}
