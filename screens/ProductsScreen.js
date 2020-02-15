807422e1efffd2874934874cedbfb914ee3a759b
Hope Tambala <hopetambala@outlook.com>
2020-02-15 09:59:58 -0500

    fix: linting

--- screens/ProductsScreen.js
+++ screens/ProductsScreen.js
@@ -2,53 +2,52 @@ import React from 'react';
 import {
   Text,
   View,
-  Button,
   ActivityIndicator,
-  FlatList
 } from 'react-native';
 import styles from '../constants/Style';
 
 class ProductsScreen extends React.Component {
   constructor(props) {
     super(props);
-      this.state = {
-        loading: true,
-        dataSource:[]
+    this.state = {
+      loading: true,
+      dataSource: []
     };
   }
 
-  componentDidMount(){
-    fetch("https://pineapple-rest-api.herokuapp.com/products")
-      .then(response => response.json())
-        .then((responseJson) => {
-          this.setState({
-            loading: false,
-            dataSource: responseJson
-        })
-      })
-    .catch(error=>console.log(error)) //to catch the errors if any
+  componentDidMount() {
+    fetch('https://pineapple-rest-api.herokuapp.com/products')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          loading: false,
+          dataSource: responseJson
+        });
+      });
   }
 
   render() {
-    if(this.state.loading){
-      return( 
-        <View style={styles.loader}> 
-          <ActivityIndicator size="large" color="#0c9"/>
+    const { dataSource, loading } = this.state;
+    if (loading) {
+      return (
+        <View style={styles.loader}>
+          <ActivityIndicator size="large" color="#0c9" />
         </View>
-    )}
+      );
+    }
 
     return (
       <View style={styles.container}>
-        <Text>I'm a ProductsScreen</Text>
+        <Text>Products Screen</Text>
         <br />
-        {this.state.dataSource.products.map((dat) =>
-        <div>
-          <li>{dat.product}</li>
-          <li>{dat.price}</li>
-        </div>
-        )}
-        
-     </View>
+        {dataSource.products.map((dat) => (
+          <div>
+            <li>{dat.product}</li>
+            <li>{dat.price}</li>
+          </div>
+        ))}
+
+      </View>
     );
   }
 }
