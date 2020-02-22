72d9d10c9158f8e5f8963c41e3255a274e9db16f
Hope Tambala <hopetambala@outlook.com>
2020-02-22 02:52:48 -0500

    feat: add example of how to use a post call to add user
    @chance-murphy @jalinp

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,17 +2,36 @@ import React from 'react';
 import { Text, View, Button } from 'react-native';
 import styles from '../constants/Style';
 
-const HomeScreen = (props) => {
-  const { navigation } = props;
-  return (
-    <View style={styles.container}>
-      <Text>This is the homescreen!</Text>
-      <Button
-        title="SCAN"
-        onPress={() => navigation.navigate('Scanner')}
-      />
-    </View>
-  );
+class HomeScreen extends React.Component {
+  componentDidMount = async() => {
+    const rawResponse = await fetch('https://pineapple-rest-api.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({username: "TestDoser", password: 'password'})
+    }).catch(err =>{
+      console.log('caught it!',err);
+    });
+
+    const content = await rawResponse.json();
+
+    console.log(content.message);
+
+  }
+  render(){
+    const { navigation } = this.props;
+
+    return (
+      <View style={styles.container}>
+        <Text>This is the homescreen!</Text>
+        <Button
+          title="SCAN"
+          onPress={() => navigation.navigate('Scanner')}
+        />
+      </View>
+    )};
 };
 
 export default HomeScreen;
